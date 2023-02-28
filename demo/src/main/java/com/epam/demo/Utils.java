06be56b836df0cc3d30a9eae466cadbbc4ebb034
Staluks <janchukssss@inbox.lv>
2023-02-28 16:24:19 +0200

    gradle task attempt 1st.

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        boolean ok = true;
+        for(String i : args){
+            if(Integer.parseInt(i)<0){
+                ok = false;
+            }
+        }
+        return ok;
     }
 }
